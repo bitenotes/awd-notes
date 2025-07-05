0666c2c3c205d98c4f435b8b0b602a6a1ee1bf0f
bitenotes <clubbite9@gmail.com>
2025-07-05 11:08:57 +0530

    Add files via upload

---
+++ Calculator/Calculator.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Calculator.aspx.cs" Inherits="AkWebApp.WebForm2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Enter Number: <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        </div>
+        <div>
+            Enter Number: <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        </div>
+        <div>
+            <asp:Button ID="Add" runat="server" Text="Add" OnClick="Add_Click" />
+            <asp:Button ID="Sub" runat="server" Text="Subtract" OnClick="Sub_Click"/>
+            <asp:Button ID="Mul" runat="server" Text="Multiply" OnClick="Mul_Click"/>
+            <asp:Button ID="Div" runat="server" Text="Divide" OnClick="Div_CLick"/>
+            <asp:Button ID="Mod" runat="server" Text="Modulus" OnClick="Mod_Click"/>
+        </div>
+        <div>
+            Addition: <asp:Label ID="Add1" runat="server" Text="_"></asp:Label><br />
+            Subtraction: <asp:Label ID="Sub1" runat="server" Text="_"></asp:Label><br />
+            Multiplication: <asp:Label ID="Mul1" runat="server" Text="_"></asp:Label><br />
+            Division: <asp:Label ID="Div1" runat="server" Text="_"></asp:Label><br />
+            Modulus: <asp:Label ID="Mod1" runat="server" Text="_"></asp:Label><br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Calculator/Calculator.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AkWebApp
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Add_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(TextBox1.Text);
+            int b = Convert.ToInt32(TextBox1.Text);
+            int c = a + b;
+            Add1.Text = c.ToString();
+        }
+        protected void Sub_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(TextBox1.Text);
+            int b = Convert.ToInt32(TextBox2.Text);
+            int c = a - b;
+            Sub1.Text = c.ToString();
+        }
+        protected void Mul_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(TextBox1.Text);
+            int b = Convert.ToInt32(TextBox2.Text);
+            int c = a * b;
+            Mul1.Text = c.ToString();
+        }
+        protected void Mod_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(TextBox1.Text);
+            int b = Convert.ToInt32(TextBox2.Text);
+            int c = a % b;
+            Mod1.Text = c.ToString();
+        }
+
+        protected void Div_CLick(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(TextBox1.Text);
+            int b = Convert.ToInt32(TextBox2.Text);
+            int c = a / b;
+            Div1.Text = c.ToString();
+        }
+    }
+}
